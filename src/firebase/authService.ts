f2b132c8af291500ce41a8b1b224895fa8496a1e
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-01-29 17:42:34 +1300

    Added temporary alert for login errors

--- src/firebase/authService.ts
+++ src/firebase/authService.ts
@@ -10,6 +10,7 @@ const login = async (auth: Auth, email: string, password: string) => {
     await signInWithEmailAndPassword(auth, email, password);
   } catch (error: any) {
     console.log(`Firbase Auth Login failed: ${error.message}`);
+    alert(error.message);
   }
 };
 
